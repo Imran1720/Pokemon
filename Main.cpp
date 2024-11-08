f89fc80745ec9f06d0ac67a388a7a5de8b10633c
Imran1720 <skimran1720@gmail.com>
2024-11-08 21:10:50 +0530

    Professor Oak’s Challenge:
    added introduction and functionality to greet player by taking user name

--- Main.cpp
+++ Main.cpp
@@ -4,6 +4,38 @@ using namespace std;
 
 int main()
 {
+    system("cls");
+
+    string pokemonTrainer_Name;
+
+    for (int i = 0; i < 45; i++)
+    {
+        cout << "*";
+    }
+
+    cout << "\n\t\tPOKEMON" << endl;
+
+    for (int i = 0; i < 45; i++)
+    {
+        cout << "*";
+    }
+    cout << "\n\nTrainer! Before you embark on your journey," << endl;
+    cout << "the Pokemon world needs to know who you are!" << endl;
+    cout << "\nTime to step up and tell us your name." << endl;
+    cout << "\nAnd remember, a great name is the start of " << endl;
+    cout << "every great adventure!" << endl << endl;
+
+    for (int i = 0; i < 45; i++)
+    {
+        cout << "-";
+    }
+
+    cout << "\n\nEnter your name: ";
+    cin >> pokemonTrainer_Name;
+
+    system("cls");
+    cout << "\nGreat start, " << pokemonTrainer_Name << "! Looks like you've mastered the opening of every code adventure."<<endl;
+    cout << "Keep this up, and you'll be a coding master in no time!"<<endl;
 	
 	return 0;
 }
