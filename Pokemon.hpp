2bd07818930c53bba0386797eea530c14f0d23a7
Imran1720 <skimran1720@gmail.com>
2024-11-14 16:50:07 +0530

    Refactor Pokemon classs

---
+++ Pokemon.hpp
@@ -0,0 +1,16 @@
+#include <string>
+#include "PokemonType.hpp"
+
+class Pokemon
+{
+public:
+    string pokemonName;
+    PokemonType typeOfPokemon;
+    int health;
+
+    Pokemon();
+    Pokemon(string _name, PokemonType _type, int _heatlh);
+    Pokemon(const Pokemon& _pokemon);
+    void Attack();
+};
+
